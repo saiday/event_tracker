f4d7a1caac830a1f05a7b0c001850fd7768199a7
Saiday <feocms@gmail.com>
2019-04-21 04:22:07 +0800

    event model added

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Event(models.Model):
+    key = models.CharField(max_length=50)
+    value = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+    confirmed_user = models.ManyToManyField(User)
