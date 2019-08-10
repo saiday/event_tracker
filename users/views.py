4063913eee82a8908d56acd99c8c65475360903d
Saiday <feocms@gmail.com>
2019-08-10 22:41:23 +0800

    profile

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 
@@ -15,3 +16,8 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
