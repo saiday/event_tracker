602e96c3a54d2ddf0059779672b7e29ec4abbbb8
Saiday <feocms@gmail.com>
2019-08-18 15:54:39 +0800

    User Profile

--- event_tracker/urls.py
+++ event_tracker/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+
+from event_tracker import settings
 from users import views as user_views
 
 urlpatterns = [
@@ -26,3 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('tracker.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
